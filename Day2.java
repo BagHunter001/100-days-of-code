92824e51c37f3ddfcb94a2e671acca9fa2b8ada0
BagHunter001 <abuyawayne72@gmail.com>
2024-07-27 21:10:17 +0300

    Variables and Data Types

---
+++ Day2.java
@@ -0,0 +1,19 @@
+public class Day2 {
+
+        public static void main(String[] args) {
+            // initialize an integer variable
+            int jInt = 25;
+            
+            //initialize a floating-point number (double) variable
+            double jDouble = 25.99;
+            
+            // initialize a string variable
+            String jString = "Hello, World!";
+            
+            // Print each variable
+            System.out.println("Integer value: " + jInt);
+            System.out.println("Double value: " + jDouble);
+            System.out.println("String value: " + jString);
+        }
+    }
+
